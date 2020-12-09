18e489e7ee9b2607fefcc38a5df930a605623dbf
cedis81 <ccchom11@gmail.com>
2020-12-09 16:11:49 -0500

    Add brain logo
    * Upload brain.png for logo use
    * Import brain.png for logo use
    * Add brain.png to Logo.js
    * Update styling for logo image

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Tilt from 'react-tilt'
 import './Logo.css';
+import brain from './brain.png';
 
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
-      <Tilt className="Tilt br2 shadow" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
-       <div className="Tilt-inner"> 👽 </div>
+      <Tilt className="Tilt br2 shadow" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+       <div className="Tilt-inner pa3">
+        <img style={{paddingTop: '5px'}} alt='logo' src={brain} />
+      </div>
       </Tilt>
+      {/*<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>*/}
     </div>
   );
 }
