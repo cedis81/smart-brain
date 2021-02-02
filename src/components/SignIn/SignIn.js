944fa293e4ed5dcc64a8e1f4b257236e07dae69d
cedis81 <ccchom11@gmail.com>
2021-02-02 18:06:12 -0500

    Remove localhost
    * Update localhost to heroku address

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://pacific-ravine-90617.herokuapp.com/signin', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json'},
